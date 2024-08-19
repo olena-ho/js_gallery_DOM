d1388537fa7f2bf8f92ba7fc61340fcc59b009ad
Olena Horovenko <elena.gorovenko@gmail.com>
2024-08-19 17:36:19 +0200

    make build

---
+++ index.78362a4b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg"),t=document.querySelector(".gallery__list");t.addEventListener("click",function(r){r.preventDefault();var c=r.target.closest("a");c&&t.contains(c)&&(e.src=c.href)});
+//# sourceMappingURL=index.78362a4b.js.map
